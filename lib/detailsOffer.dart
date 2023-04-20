2669f50fc706d2f211c11b29185bbdd7d1229029
maleekhamza <maleek.hm29@gmail.com>
2023-04-21 00:22:28 +0200

    detalis

--- lib/detailsOffer.dart
+++ lib/detailsOffer.dart
@@ -1,18 +1,25 @@
 import 'package:chercher_job/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class detailsOffer extends StatefulWidget {
-  const detailsOffer({Key? key}) : super(key: key);
+  detailsOffer({Key? key}) : super(key: key) {
+    _reference = FirebaseFirestore.instance.collection('posts').doc();
+    _futureData = _reference.get();
 
+  }
+  late DocumentReference _reference;
+  late Future<DocumentSnapshot> _futureData;
   @override
   State<detailsOffer> createState() => _detailsOfferState();
 }
 
 class _detailsOfferState extends State<detailsOffer> {
+   Map<dynamic, dynamic> data= {};
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  Scaffold(
       appBar: AppBar(
         title: Text(
           "Details",
@@ -36,7 +43,22 @@ class _detailsOfferState extends State<detailsOffer> {
             onPressed: () {}),
 
       ],
-      ),
+        // actions: [
+        //  IconButton(
+        //onPressed: () {
+        //add the id to the map
+        // data['id'] = itemId;
+        // },
+        //icon: Icon(Icons.edit)),
+        //IconButton(onPressed: (){
+        //Delete the item
+        //_reference.delete();
+        // }, icon: Icon(Icons.delete))
+        // ],
+      ),    //,
+
     );
   }
+
 }
+
